568ba961b9869b0a7e957cd95f5fe3f17ab1b14b
Joys Princia <joysprincia9906@gmail.com>
2022-03-22 11:03:25 +0530

    initial commit

---
+++ collegemgmt/src/main/java/com/example/collegemgmt/CollegemgmtApplication.java
@@ -0,0 +1,13 @@
+package com.example.collegemgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollegemgmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollegemgmtApplication.class, args);
+	}
+
+}
